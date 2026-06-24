f50a83e966db84a21a0d3032a9d643d689e0f79c
Nick Peoples <nick@nicknet.xyz>
2026-06-23 23:35:57 -0400

    Expose host override id as output

--- modules/opnsense-dns/outputs.tf
+++ modules/opnsense-dns/outputs.tf
@@ -1,4 +1,9 @@
 # modules/opnsense-dns/outputs.tf
+output "id" {
+  description = "ID of the host override resource."
+  value       = opnsense_unbound_host_override.this.id
+}
+
 output "hostname" {
   description = "The hostname configured for this record."
   value       = opnsense_unbound_host_override.this.hostname
